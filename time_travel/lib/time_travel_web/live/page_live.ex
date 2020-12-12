9b0df473f0dc3d4485123f0400eb93e39ca289ed
Adam Lancaster <lancaster28@hotmail.com>
2020-12-12 17:35:46 +0000

    Demonstrate an event firing
    The demo can then show pattern matching on that event and doing
    something based on it.

--- time_travel/lib/time_travel_web/live/page_live.ex
+++ time_travel/lib/time_travel_web/live/page_live.ex
@@ -1,16 +1,21 @@
 defmodule TimeTravelWeb.PageLive do
   use TimeTravelWeb, :live_view
+  require Logger
 
   @impl true
   def mount(_params, _session, socket) do
     assigns = %{
       create_modal: false,
-      tickets: []
+      tickets: [%{name: "ted Bundy", destination: "1973", departure: "now"}]
     }
     {:ok, assign(socket, assigns)}
   end
 
   @impl true
-  def handle_event("create", %{"q" => query}, socket) do
+  def handle_event(event, params, socket) do
+    event |> IO.inspect(limit: :infinity, label: "EVENT")
+    params |> IO.inspect(limit: :infinity, label: "PARAMS")
+    Logger.warn("Unrecognised event triggered!")
+    {:noreply, socket}
   end
 end
